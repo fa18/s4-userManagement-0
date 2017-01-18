c6e5134967518d48472d66b8c0624210541f7b80
Fabien Fontaine <Fabien Fontaine>
2017-01-18 15:11:54 +0100

    I

--- roles/index.php
+++ roles/index.php
@@ -1,4 +1,14 @@
+<meta charset=utf-8>
 <?php 
     //Connexion à la BDD
-    include("../includes/connexionBDD.php");
+    include("../includes/connexionBDD.php"); echo '</br>';
+
+    $ch = "SELECT name, count(idrole) as nombre, role.id FROM role LEFT JOIN user ON user.idrole = role.id GROUP BY role.id, name";
+    $tab = LireDonneesPDO1($conn,$ch);
+    AfficherDonnee($tab);
+
+
+
+ 	 
+ 
 ?>
